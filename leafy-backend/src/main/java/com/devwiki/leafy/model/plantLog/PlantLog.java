abc204df5b81a7b80c38e92a37712721be44b333
devwiki-henry <devwiki.henry@gmail.com>
2024-05-04 22:04:46 +0900

    Spring Boot 3.0

--- leafy-backend/src/main/java/com/devwiki/leafy/model/plantLog/PlantLog.java
+++ leafy-backend/src/main/java/com/devwiki/leafy/model/plantLog/PlantLog.java
@@ -3,7 +3,7 @@ package com.devwiki.leafy.model.plantLog;
 import com.devwiki.leafy.dto.plantLog.PlantLogDto;
 import com.devwiki.leafy.model.userPlant.UserPlant;
 import lombok.*;
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
--- leafy-backend/src/main/java/com/devwiki/leafy/model/user/User.java
+++ leafy-backend/src/main/java/com/devwiki/leafy/model/user/User.java
@@ -7,8 +7,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
+import jakarta.persistence.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 
